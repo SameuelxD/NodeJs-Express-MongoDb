7de32a2b87d2ffb0a384247da1d95331ccd51d01
Ing.Samuel <126287892+SameuelxD@users.noreply.github.com>
2024-01-04 16:56:57 -0500

    Adding Method Put , because if want changes in the data

--- Servidor/Router/Mascotas.js
+++ Servidor/Router/Mascotas.js
@@ -76,8 +76,22 @@ router.delete('/:id', async (req, res) => {
                 mensaje: 'fallo eliminar!'
             })
         }
-    }catch(error){
+    } catch (error) {
         console.log(error);
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const body = req.body
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false }
+        )
+        console.log(mascotaDB);
+        res.json({ estado: true, mensaje: 'Editado' })
+    } catch (error) {
+        console.log(error);
+        res.json({ estado: false, mensaje: 'Fallamos' })
     }
 })
 
